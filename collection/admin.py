b3e8d75aaba06c6b20d3ed129ddd79bd7c678c9b
RDavis2005 <RDavis2005@msn.com>
2018-11-14 13:24:01 -0500

    Chapters 6 Database Completed

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+#Import Your Model
+from collection.models import Thing
+
+#Set Up Automated Slug Creation
+class ThingAdmin(admin.ModelAdmin):
+    model = Thing
+    list_display = ('name', 'description',) #Tells Django that we want this to show in admin
+    prepopulated_fields = {'slug': ('name',)} #Prepopulate the slug field based off of the name field when someone types the name of a thing in the admin
+
+#Register Your Model
+admin.site.register(Thing, ThingAdmin)
